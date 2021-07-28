9d009074a756bdc64aa045581a2815e017fc3aaf
Roman Linevych <linevyhroman@gmail.com>
2021-07-28 19:05:52 +0300

    fix burger menu in header

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,12 @@
+const menuButton = document.getElementById('menuBtn');
+const menuList = document.getElementById('navList');
+const menuLineTop = document.getElementById('menuLineTop');
+const menuLineCenter = document.getElementById('menuLineCenter');
+const menuLineBottom = document.getElementById('menuLineBottom');
 
-
-$(function () {
-
-    $('.header__btn').on('click', function () {
-        $('.header__nav').toggleClass('header__nav--active')
-    });
-});
+menuButton.onclick = function () {
+    menuList.classList.toggle('nav__list_active');
+    menuLineTop.classList.toggle('menu__line-top_active');
+    menuLineCenter.classList.toggle('menu__line-center_active');
+    menuLineBottom.classList.toggle('menu__line-bottom_active');
+}
