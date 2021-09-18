5c758e4cac65838970ebbb23eb7de5fda417647e
Roman Linevych <linevyhroman@gmail.com>
2021-09-18 11:31:09 +0300

    create project

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,12 @@
-const menuButton = document.getElementById('menuBtn');
+const menuButton = document.getElementById('burgerBtn');
 const menuList = document.getElementById('navList');
-const menuLineTop = document.getElementById('menuLineTop');
-const menuLineCenter = document.getElementById('menuLineCenter');
-const menuLineBottom = document.getElementById('menuLineBottom');
+const menuLineTop = document.getElementById('burgerBtnLineTop');
+const menuLineCenter = document.getElementById('burgerBtnLineCenter');
+const menuLineBottom = document.getElementById('burgerBtnLineBottom');
 
 menuButton.onclick = function () {
     menuList.classList.toggle('nav__list_active');
-    menuLineTop.classList.toggle('menu__line-top_active');
-    menuLineCenter.classList.toggle('menu__line-center_active');
-    menuLineBottom.classList.toggle('menu__line-bottom_active');
+    menuLineTop.classList.toggle('burger-btn__line-top_active');
+    menuLineCenter.classList.toggle('burger-btn__line-center_active');
+    menuLineBottom.classList.toggle('burger-btn__line-bottom_active');
 }
